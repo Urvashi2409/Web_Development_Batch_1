0fa7be47e24f9e4b1e3afeb7b74f5df5d6dfef67
urvashi2410 <urvashianand0906@gmail.com>
2023-07-15 15:35:47 +0530

    all classes of batch-1 till now

---
+++ Javascript/script.js
@@ -0,0 +1,12 @@
+document.querySelector(".heading1").textContent = "HIIIIIIIIIIIIIIIIIIIIIIIIIIIIII";
+document.querySelector("#heading2").textContent = "H!!!!!!!!";
+
+console.log("HELLOOOOO EVERYONE GLAD MEETING YOU!")
+
+// literals and variables 
+
+// 23.4 
+// "Brian"
+
+// literals: we cannot change its value 
+// variable: we can change its value 
