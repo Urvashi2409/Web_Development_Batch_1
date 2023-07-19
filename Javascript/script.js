e7fd8b8af5bc3c14192e530fc6232ca630988ced
Urvashi2409 <urvashicode2410@gmail.com>
2023-07-20 05:28:50 +0530

    var, let, data types

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,7 +1,7 @@
-document.querySelector(".heading1").textContent = "HIIIIIIIIIIIIIIIIIIIIIIIIIIIIII";
-document.querySelector("#heading2").textContent = "H!!!!!!!!";
+// document.querySelector(".heading1").textContent = "HIIIIIIIIIIIIIIIIIIIIIIIIIIIIII";
+// document.querySelector("#heading2").textContent = "H!!!!!!!!";
 
-console.log("HELLOOOOO EVERYONE GLAD MEETING YOU!")
+// console.log("HELLOOOOO EVERYONE GLAD MEETING YOU!")
 
 // literals and variables 
 
@@ -10,3 +10,107 @@ console.log("HELLOOOOO EVERYONE GLAD MEETING YOU!")
 
 // literals: we cannot change its value 
 // variable: we can change its value 
+
+// numbers in front doesn't work 
+// let 45raymond = "raymond";
+// console.log(45raymond);
+
+// numbers in between or end works 
+// let raym45ond = "raymond";
+// console.log(raym45ond);
+
+// starting with uppercase works 
+// let RAYmond = "function";
+// console.log(RAYmond);
+
+// small case also works 
+// let name = "raymond";
+// console.log(name);
+
+// you cannot use reserved keywords 
+// let for = 34;
+// console.log(for);
+
+// variable can be as long as possible 
+// let aakjsfasdgfsgdfksdfgkdsfgsdsdkfsdkfgsdkfgfdsgfgskgdfgdffkjfdjfsdf4743287424834 = 34;
+// console.log(aakjsfasdgfsgdfksdfgkdsfgsdsdkfsdkfgsdkfgfdsgfgskgdfgdffkjfdjfsdf4743287424834);
+
+// only $ and _ works as symbols in variables 
+// let $_34rtl = "Brian";
+// console.log($_34rtl);
+
+// let %ythis = 45;
+// console.log(%ythis);
+
+// data types 
+// let a = 23;
+// console.log(typeof a);
+
+// let a = 'r';
+// console.log(typeof a);
+
+// let a = "hello world";
+// console.log(typeof a);
+
+// let a = true;
+// console.log(typeof a);
+
+// let a = 45.6;
+// console.log(typeof a);
+
+// let a = 45.73284238344437824238446238462347437;
+// console.log(typeof a);
+
+// let a;
+// console.log(typeof a);
+
+// let a = null;
+// console.log(typeof a);
+
+// to show bigint data type you need to add 'n' at the end of the number 
+// let a = 17899999999999999637689952297262604707763763781924021995402759317737096166765929102732906163840610893143733352942093575278589544416123407498484317896261917232629524426272214176638262229922362643847008815021898799795474786619818468662801396611976926115098855495297046201853378792672517656002125878565687158374434564364534535n;
+// console.log(typeof a);
+
+// let vs const vs var 
+// let a = 34;
+// console.log(a);
+
+// a = 56;
+// console.log(a);
+
+// var r = 34;
+// console.log(r);
+
+// r = 56;
+// console.log(r);
+
+// const p = 34;
+// console.log(p);
+
+// p = 56;
+// console.log(p);
+
+// we can change the value of var and let but the const variable 
+
+// you can initialize the var variable even before decalaring it 
+// you can initialize the let variable only after decalaring it 
+
+// let brian;
+// brian = 10000000;
+
+// console.log(brian);
+
+// brian = 10000000;
+// var brian; 
+
+// console.log(brian);
+
+// arithematic operators 
+// +, -, *, /, % , ** 
+
+// console.log(34+3);
+// console.log(34-3);
+// console.log(34*3);
+// console.log(34/3);
+console.log(3%34);
+// console.log(34**3);
